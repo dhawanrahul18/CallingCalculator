048b382a016fdd89f5a5160c98f2174a18f4a3af
USTXPLLP01030\Rahul <rd00333996@techmahindra.com>
2018-04-24 11:19:18 -0700

    first commit by Rahul

---
+++ src/main/java/org/workspace/calculator/SwaggerConfiguration.java
@@ -0,0 +1,37 @@
+package org.workspace.calculator;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@PropertySource("classpath:swagger.properties")
+public class SwaggerConfiguration {
+
+    @Bean
+    Docket claculatorApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Claculator microservices REST API")
+                .description("Operations that can be invoked in the simple calculator microservices")
+                .contact(new Contact("Rahul Dhawan", "http://blogs.workspace7.org", "dhawanrahul18@gmail.com"))
+                .license("Apache License Version 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
+}
